87e672c9b49712ce620d5c4d7aa0cd1e98eec277
Nikita <kindpavlov@yandex.ru>
2023-03-08 21:14:42 +0300

    Решил задачу Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,3 +3,22 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите тертье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number2 > max) 
+{
+  max = number2;
+}
+if (number3 > max) 
+{
+  max = number3;
+}
+Console.WriteLine($"Максимальное число {max}" );
+
