2fd32af5953d43a6b541a37b166984c2552c39bd
Nikita <kindpavlov@yandex.ru>
2023-03-09 10:26:19 +0300

    Оптимизировал решение задачи Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -12,13 +12,8 @@ Console.WriteLine("Введите тертье число:");
 int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
 
-if (number2 > max) 
-{
-  max = number2;
-}
-if (number3 > max) 
-{
-  max = number3;
-}
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
 Console.WriteLine($"Максимальное число {max}" );
 
