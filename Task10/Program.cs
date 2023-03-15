6ccb5aa7ab374e4db9e617602eca5ccf3ea59067
Nikita <kindpavlov@yandex.ru>
2023-03-15 13:25:27 +0300

    Решил задачу Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,3 +3,24 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+  int total = SecondDigit(number);
+  Console.WriteLine($"Вторая цифра = {total}");
+}
+else
+{
+  Console.WriteLine("Введите пожалуйста трехзначное число!");
+}
+
+int SecondDigit(int num)
+{
+  int secondDigit = num / 10 % 10;
+  int result = secondDigit;
+  return result;
+}
