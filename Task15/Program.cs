6b74afcd38476fa80b58293eec00bf081edc0ee5
Nikita <kindpavlov@yandex.ru>
2023-03-16 12:06:56 +0300

    Решил задачу Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,3 +3,21 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.Clear();
+Console.WriteLine("Введите число указывающее день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 8)
+{
+  Console.WriteLine("У нас в неделе семь дней. Введите корректное число!");
+}
+else
+{
+  bool result = DayOff(number);
+  Console.WriteLine(result ? "О да, наступил выходной!"
+                            : "Работаем, негры, ещё не выходной!");
+}
+
+bool DayOff(int num)
+{
+  return num > 5 && num <= 7;
+}
