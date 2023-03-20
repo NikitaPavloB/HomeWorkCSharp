6f303cd2496bc17efc23a8afec64e048e2bfe21b
Nikita <kindpavlov@yandex.ru>
2023-03-20 18:31:25 +0300

    Убрал отрицательный вариант

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -10,9 +10,6 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 < 0) number1 = -number1;
-if (number2 < 0) number2 = -number2;
-
 int total = Exponentiation(number1, number2);
 Console.WriteLine($"Число {number1} в степени {number2} = {total}");
 
