85bd702301cfe4fc1401b0cd80428e1e9c5f7996
Nikita <kindpavlov@yandex.ru>
2023-03-20 19:51:49 +0300

    Решил задачу Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,3 +5,24 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = -number;
+
+int result = SumDigits(number);
+Console.WriteLine($"Сумма всех цифр в числе {number} = {result}");
+
+int SumDigits(int num)
+{
+  int sum = 0;
+  while (num > 0)
+  {
+    sum += num % 10;
+    num = num / 10;
+  }
+  return sum;
+}
+
