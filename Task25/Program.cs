071079fd4caa96d1b2c47f42d6e02a9b7f0ed06c
Nikita <kindpavlov@yandex.ru>
2023-03-20 13:17:44 +0300

    Решил задачу Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,3 +3,25 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 0) number1 = -number1;
+if (number2 < 0) number2 = -number2;
+
+int total = Exponentiation(number1, number2);
+Console.WriteLine($"Число {number1} в степени {number2} = {total}");
+
+int Exponentiation(int num1, int num2)
+{
+  int result = 1;
+  for (int i = 0; i < num2; i++)
+  {
+    result *= num1;
+  }
+  return result;
+}
