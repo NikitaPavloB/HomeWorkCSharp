65c423fe82dab40be4d022c85c66dfe4fe3f1f80
Nikita <kindpavlov@yandex.ru>
2023-04-06 13:12:34 +0300

    Решил задачу Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -2,3 +2,40 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+int NumberM = ReadPositiveInteger("Введите целое положительное число M: ");
+int NumberN = ReadPositiveInteger("Введите целое положительное число N: ");
+
+int result = AckermannFunction(NumberM, NumberN);
+Console.WriteLine($"Результат функции Аккермана для чисел {NumberM} и {NumberN}: {result}");
+
+static int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+
+int ReadPositiveInteger(string message)
+{
+  while (true)
+  {
+    Console.Write(message);
+    if (int.TryParse(Console.ReadLine(), out int result) && result > 0)
+    {
+      return result;
+    }
+    Console.WriteLine("Ошибка! Введите положительное целое число.");
+  }
+}
